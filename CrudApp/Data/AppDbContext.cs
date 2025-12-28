640910ca51acc0d9e66790fbbe1c562c9b95702b
Rubelhabib <rubelhabib007@gmail.com>
2025-12-28 12:19:19 +0600

    Added class product and Data

---
+++ CrudApp/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CrudApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace CrudApp.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
