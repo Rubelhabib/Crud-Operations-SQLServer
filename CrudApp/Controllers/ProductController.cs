b7963b5a2b96ee7efa123c022b7cc1c350005d55
Rubelhabib <rubelhabib007@gmail.com>
2025-12-29 23:06:43 +0600

    Added edit, Create

---
+++ CrudApp/Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using CrudApp.Data;
+using CrudApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> products = _context.Products.ToList();
+            return View(products);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create([Bind("Name, Price, Description, Stock")] Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Add(model);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id) 
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+    }
+}
